cb7bcd4a1e46ae19afdfea2467610fa7222d8e1d
Martinus Suherman <martinus.suherman@gmail.com>
2020-10-02 19:00:35 +0700

    Set to auto redirect after logout

--- src/Skoruba.IdentityServer4.STS.Identity/Configuration/AccountOptions.cs
+++ src/Skoruba.IdentityServer4.STS.Identity/Configuration/AccountOptions.cs
@@ -15,7 +15,7 @@ namespace Skoruba.IdentityServer4.STS.Identity.Configuration
         public static TimeSpan RememberMeLoginDuration = TimeSpan.FromDays(30);
 
         public static bool ShowLogoutPrompt = true;
-        public static bool AutomaticRedirectAfterSignOut = false;
+        public static bool AutomaticRedirectAfterSignOut = true;
 
         // specify the Windows authentication scheme being used
         public static readonly string WindowsAuthenticationSchemeName = Microsoft.AspNetCore.Server.IISIntegration.IISDefaults.AuthenticationScheme;
